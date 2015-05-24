46d2cc0e9ecab6b45728cb423506aed2d6bc0613
Rit <rihierit.t@gmail.com>
2015-05-24 22:45:58 +0700

    Edit User#show && routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   resources :users
   get 'static_pages/home' => 'static_pages#home'
   root 'static_pages#home'
+  # devise_for :users
+  devise_for :users, path: "auth", path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+  resources :users
+
+  get 'user' => 'user#index'
+  get 'new_user' => 'user#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
