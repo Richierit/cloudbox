bd044907596d35d11979f6fff2ff3e068414a2db
Rit <rihierit.t@gmail.com>
2015-05-24 22:48:26 +0700

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   # devise_for :users
   devise_for :users, path: "auth", path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
-  resources :users
+
 
   get 'user' => 'user#index'
   get 'new_user' => 'user#new'
